3a4f511f81db52234cb48152b963b4a199fd9a00
ndi16mai <ndi16mai@13031-STUD-D.HIG-AD.hig.se>
2017-10-18 15:18:47 +0200

    gui

---
+++ SRU-PROJEKT/src/sru/MainGUI.java
@@ -0,0 +1,51 @@
+package sru;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+
+public class MainGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainGUI frame = new MainGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainGUI() {
+		INFOSRU info = new INFOSRU();
+		INK2R ink2r = new INK2R();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.NORTH);
+		tabbedPane.add(info);
+		tabbedPane.add(ink2r);
+		
+	}
+
+}
