519fb90700cd83cd2ba9bc995c631f1e27a67c02
Gianfranco <Gianfranco@172.16.117.47>
2017-10-20 13:33:16 +0200

    uppdatering

--- SRU-PROJEKT/Views/sru/MainGUI.java
+++ SRU-PROJEKT/Views/sru/MainGUI.java
@@ -12,48 +12,38 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
+import javax.swing.JMenuBar;
 
 public class MainGUI extends JFrame {
 
 	private static INFOSRU info;
 	private static Writer Candidateoutput = null;
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Launch the application.
-	 * @throws IOException 
-	 */
+	
 	public static void main(String[] args) throws IOException {
 		
 		File Candidatefile = new File("info.sru");
-		Candidateoutput = new BufferedWriter(new FileWriter(Candidatefile));
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+		
 					MainGUI frame = new MainGUI();
 					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+				
+			
+		
 	}
 
-	/**
-	 * Create the frame.
-	 * @throws IOException 
-	 */
+	
 	public MainGUI() throws IOException {
 		
 		JPanel contentPane;
 		
 		info = new INFOSRU();
-		INK2R ink2r = new INK2R();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 677);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -62,7 +52,7 @@ public class MainGUI extends JFrame {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		contentPane.add(tabbedPane, BorderLayout.NORTH);
 		tabbedPane.add(info);
-		tabbedPane.add(ink2r);
+		
 		
 		
 	}
---
+++ SRU-PROJEKT/controllers/sru/infoSruController.java
@@ -0,0 +1,30 @@
+package sru;
+
+public class infoSruController {
+	private String produkt, mediaId, skapad, program, filnamn, orgnr,
+	namn, adress, postnr,postort, avdelning,kontakt,email,telefon,fax;
+	
+	
+	public boolean isCorrectInfoSru(String produkt, String mediaId, String skapad, String program, String filnamn,
+			String orgnr, String namn, String adress, String postnr, String postort, String avdelning, String kontakt,
+			String email, String telefon, String fax) {
+		if(orgnr.length()€	))
+		this.produkt = produkt;
+		this.mediaId = mediaId;
+		this.skapad = skapad;
+		this.program = program;
+		this.filnamn = filnamn;
+		this.orgnr = orgnr;
+		this.namn = namn;
+		this.adress = adress;
+		this.postnr = postnr;
+		this.postort = postort;
+		this.avdelning = avdelning;
+		this.kontakt = kontakt;
+		this.email = email;
+		this.telefon = telefon;
+		this.fax = fax;
+	}
+	
+	
+}
